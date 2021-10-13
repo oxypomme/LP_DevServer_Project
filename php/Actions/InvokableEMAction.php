625eb5f8a0fd2348661b51928fd4956b57bb726f
SUBLET Tom <oxytom8@gmail.com>
2021-10-13 09:45:57 +0200

    refactor(api): :fire: Removed InvokableEMAction::getFullObject
    InvokableEMAction::getFullObject was deprecated and wasn't used anymore. See #64c1cc3

--- php/Actions/InvokableEMAction.php
+++ php/Actions/InvokableEMAction.php
@@ -8,14 +8,6 @@ abstract class InvokableEMAction extends InvokableJSONAction
 {
   protected EntityManager $em;
 
-  /**
-   * @deprecated Use \Crisis\Reflection::getFullObject instead, or just don't use it
-   */
-  protected function getFullObject(object $obj, array $excluded = []): object
-  {
-    return \Crisis\Reflection::getFullObject($obj, $excluded);
-  }
-
   public function __construct(\UMA\DIC\Container $cnt)
   {
     $this->em = $cnt->get(\Doctrine\ORM\EntityManager::class);
