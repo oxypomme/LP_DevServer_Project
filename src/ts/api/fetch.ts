74da970d7ff960666b0ef170cf629222b17095c2
SUBLET Tom <oxytom8@gmail.com>
2021-10-15 10:47:31 +0200

    perf: :construction: Added types to API Endpoints

--- src/ts/api/fetch.ts
+++ src/ts/api/fetch.ts
@@ -1,15 +1,36 @@
 type HttpMethods = "GET" | "POST" | "PUT" | "DELETE";
 
-type ApiEndoints = string;
+type AuthEndpoints = "auth";
 
-type ApiInput = `/api/${ApiEndoints}`;
+type BaseGroupEndpoints = "groups" | `groups/${number}`;
 
-const fetchAPI = (
-  endpoint: ApiInput,
-  method: HttpMethods = "GET",
-  init?: RequestInit
-) => {
-  return fetch(`/api/${endpoint}`, {
+type RelationEndpoints = "relations" | `relations/${number}`;
+
+type MessagesEndpoints = "messages" | `messages/${number}`;
+
+type UserEndpoints =
+  | "users"
+  | `users/${number}`
+  | `users/${number}/${
+      | BaseGroupEndpoints
+      | RelationEndpoints
+      | "location"
+      | MessagesEndpoints}`;
+
+type GroupMembersEndpoints = "members" | `members/${number}`;
+
+type GroupEndpoints =
+  | BaseGroupEndpoints
+  | `groups/${number}/${GroupMembersEndpoints | MessagesEndpoints}`;
+
+type ApiEndoints = AuthEndpoints | UserEndpoints | GroupEndpoints;
+
+type ApiInput = `${HttpMethods} /api/${ApiEndoints}`;
+
+const fetchAPI = (endpoint: ApiInput, init?: RequestInit) => {
+  const [, method, path] = endpoint.match(/(?<method>\S*)\s(?<path>\S*)/) ?? [];
+
+  return fetch(path, {
     method,
     headers: {
       Authorization: "Bearer " + localStorage.getItem("authToken"),
