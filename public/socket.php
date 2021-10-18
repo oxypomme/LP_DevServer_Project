4d74751a966f0f68bec851931404aa9ae9e920a8
OxyTom <oxytom8@gmail.com>
2021-10-18 12:51:04 +0200

    feat: :construction: Started Server Websockets

--- public/socket.php
+++ public/socket.php
@@ -6,15 +6,40 @@ use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
 use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
+use \Crisis\JSON;
 
 class ServerImpl implements MessageComponentInterface
 {
-  protected $clients;
+  /** @var \SplObjectStorage<ConnectionInterface> $clients */
+  protected \SplObjectStorage $clients;
+
+  protected \Doctrine\ORM\EntityManager $em;
+
+  protected array $settings;
 
   public function __construct(\UMA\DIC\Container $cnt)
   {
     $this->clients = new \SplObjectStorage;
     $this->em = $cnt->get(\Doctrine\ORM\EntityManager::class);
+    $this->settings = $cnt->get('settings');
+  }
+
+  public function checkJWT(string $jwt): bool
+  {
+    $factory = new \PsrJwt\Factory\Jwt();
+    $parser = $factory->parser($jwt, $this->settings['jwt']['secret']);
+    try {
+      $parser->validate();
+    } catch (\ReallySimpleJWT\Exception\ValidateException $th) {
+      return false;
+    }
+    $parsed = $parser->parse();
+
+    $auth_user_id = (int) $parsed->getSubject();
+    $user = $this->em
+      ->getRepository(\Crisis\Models\User::class)
+      ->find($auth_user_id);
+    return !is_null($user);
   }
 
   public function onOpen(ConnectionInterface $conn)
@@ -23,14 +48,33 @@ class ServerImpl implements MessageComponentInterface
     echo "New connection! ({$conn->resourceId}).\n";
   }
 
-  public function onMessage(ConnectionInterface $conn, $msg)
+  public function onMessage(ConnectionInterface $conn, $inMsg)
   {
+    $event = json_decode($inMsg);
+    // TODO check $event->jwt
+    if (!$this->checkJWT($event->jwt)) {
+      $conn->send(JSON::encode([
+        'type' => "error",
+        "payload" => "Auth Failed"
+      ]));
+      return;
+    }
+    unset($event->jwt);
+
+    $msg = JSON::encode($event);
     echo sprintf("New message from '%s': %s\n\n\n", $conn->resourceId, $msg);
-    foreach ($this->clients as $client) { // BROADCAST
-      $message = json_decode($msg, true);
-      if ($conn !== $client) {
-        $client->send($msg);
-      }
+    switch ($event->type) {
+      case 'ping':
+        $conn->send($msg);
+        break;
+
+      default:
+        foreach ($this->clients as $client) { // BROADCAST
+          // if ($conn !== $client) {
+          $client->send($msg);
+          // }
+        }
+        break;
     }
   }
 
